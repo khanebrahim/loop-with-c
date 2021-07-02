6ad8c244ec4dc0b93953c95c08b7d5c1af4bc4c2
BengalDroid <bengaldroid@gmail.com>
2021-07-02 10:01:35 +0600

    while

--- addition.c
+++ addition.c
@@ -1,14 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    printf("1\n");
-    printf("2\n");
-    printf("3\n");
-    printf("4\n");
-    printf("5\n");
-    printf("6\n");
-    printf("7\n");
-    printf("8\n");
-    printf("9\n");
-    printf("10\n");
+    int n=1;
+    while(n<=10){
+        printf("%d\n",n);
+        n++;
+    }
+    return 0;
+
 }
