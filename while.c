07da63347a7dcf6c455994179eca59f657083f1d
BengalDroid <bengaldroid@gmail.com>
2021-07-02 11:37:39 +0600

    for loop

---
+++ while.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n=5;
+    int i=1;
+    while(i<=10){
+        printf("%d * %d =%d\n",n,i,n*i);
+        i=i+1;
+    }
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
